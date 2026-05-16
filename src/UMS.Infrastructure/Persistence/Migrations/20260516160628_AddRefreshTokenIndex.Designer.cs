36f6c775628f4267a616ec95d060c21eb095cd1f
yob mon <eyobsolomon33@gmail.com>
2026-05-16 09:18:49 -0700

    indexed RefreshToken for fast lookup

--- src/UMS.Infrastructure/Persistence/Migrations/20260516160628_AddRefreshTokenIndex.Designer.cs
+++ src/UMS.Infrastructure/Persistence/Migrations/20260516160628_AddRefreshTokenIndex.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UMS.Infrastructure.Persistance.Context;
@@ -11,9 +12,11 @@ using UMS.Infrastructure.Persistance.Context;
 namespace UMS.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260516160628_AddRefreshTokenIndex")]
+    partial class AddRefreshTokenIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
