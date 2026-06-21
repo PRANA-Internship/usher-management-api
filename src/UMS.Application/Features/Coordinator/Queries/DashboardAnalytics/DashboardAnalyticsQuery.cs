6ffc2a444f9e1a1ca33428d364120709a816642e
Dawit Solomon <dawit8908@gmail.com>
2026-06-21 11:10:06 +0300

    fix: code formatted

--- src/UMS.Application/Features/Coordinator/Queries/DashboardAnalytics/DashboardAnalyticsQuery.cs
+++ src/UMS.Application/Features/Coordinator/Queries/DashboardAnalytics/DashboardAnalyticsQuery.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using MediatR;
+
 using UMS.Application.Common.Interfaces;
 using UMS.Contracts.Coordinator;
 using UMS.Domain.Common;
